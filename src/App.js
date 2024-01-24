aae8552b1002ee7a4c5824d92f086ea9dce4d068
JeongUn <wjddns23050@gmail.com>
2024-01-24 10:33:10 +0900

    코드 수정 및 배포 확인

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>App js</code>
+          <code>Deploy Test</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
