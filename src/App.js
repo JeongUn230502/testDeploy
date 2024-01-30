05185d91d28ee5929d394ab07dbd432c64e44694
JeongUn <wjddns23050@gmail.com>
2024-01-30 17:01:04 +0900

    Docker init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>React App to Docker Test</code>
+          <code>Dataly docker aws test</code>
         </p>
       </header>
     </div>
